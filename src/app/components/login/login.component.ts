d78c667daccbd19882d0c2a3e0fef4fa0fca6eb8
Stefan474 <Fanste474@gmail.com>
2025-06-12 16:32:08 +0200

    add FE form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,36 +1,35 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 import { Router } from '@angular/router';
-
-
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  imports: [FormsModule]
+  imports: [FormsModule, NgIf]
 })
 export class LoginComponent {
-    email = '';
-    password = '';
+  email = '';
+  password = '';
 
-    constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
-    handleLogin(): void {
-      this.authService.login(this.email, this.password).subscribe({
-        next: (response) => {
-          const token = response.token;
-          if (token) {
-            localStorage.setItem('authToken', token);
-            console.log('Login successful. Token stored:', token);
-            this.router.navigate(['/diary']);
-          }
-        },
-        error: (error) => {
-          console.error('Login error:', error);
-        },
-      });
-    }
+  handleLogin(): void {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        const token = response.token;
+        if (token) {
+          localStorage.setItem('authToken', token);
+          console.log('Login successful. Token stored:', token);
+          this.router.navigate(['/diary']);
+        }
+      },
+      error: (error) => {
+        console.error('Login error:', error);
+      },
+    });
+  }
 
   handleRegister(): void {
     this.authService.register(this.email, this.password).subscribe({
